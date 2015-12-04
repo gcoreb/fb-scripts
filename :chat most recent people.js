dcef92c409bb4f05172501238e7f2c8198949485
Bryan Chen <brybryc.music@gmail.com>
2015-12-03 17:02:04 -0800

    yes

---
+++ :chat most recent people.js
@@ -0,0 +1,14 @@
+//chat most recent people
+var chatBox  = document.getElementsByName("message_body")[0];
+var boxID = document.getElementById("u_jsonp_12_6"/*seeDesc*/);
+var spamInit = function(){
+for(var i =0; i<20/*seeDesc*/; i++){
+
+ document.getElementsByClassName('_k_')[i].click();
+chatBox.value = ''e";
+boxID.click();
+
+};
+}
+window.onload = spamInit();
+
